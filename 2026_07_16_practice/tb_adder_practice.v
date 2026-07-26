9686d4dbacfbfdbd409148242e8fd55ccee07c9c
hypeedgelighter-coder <hypeedgelighter@gmail.com>
2026-07-27 08:21:35 +0900

    Add tb_adder_practice.v (merged from 26ai_camp)

---
+++ 2026_07_16_practice/tb_adder_practice.v
@@ -0,0 +1,35 @@
+`timescale 1ns / 1ps
+
+
+
+module tb_adder_practice();
+reg [7:0]a,b;
+wire [8:0] s;
+integer i,j;
+
+full_adder_8bit dut(
+    .a(a),
+    .b(b),
+    .s(s)
+
+);
+initial begin
+    a=8'b0;
+    b=8'b0;
+    i=0;
+    j=0;
+
+    for (i=0;i<256;i=i+1)begin
+        for (j=0;j<256;j=j+1)begin
+            a=i;
+            b=j;
+            #10;
+        end
+        #10;
+    end
+
+        $stop;
+end
+    
+
+endmodule
