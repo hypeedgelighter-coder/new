553be739458db001bd19d4cb7411b9cb01e8d9d2
hypeedgelighter-coder <hypeedgelighter@gmail.com>
2026-07-27 09:14:48 +0900

    Add 202.v to 01_project_1 (verilog only)

---
+++ 01_project_1/202.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 2026/07/21 20:37:39
+// Design Name: 
+// Module Name: 202
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module 202(
+
+    );
+endmodule
